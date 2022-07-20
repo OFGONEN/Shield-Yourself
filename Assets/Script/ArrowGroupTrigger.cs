711d0a236f1860f4be02b3eec2dd5b1f32777ac4
ofgonen <ofgonen@gmail.com>
2022-07-20 15:50:46 +0300

    Implement ArrowGroupTrigger
    - Configure arrow_trigger.prefab

--- Assets/Script/ArrowGroupTrigger.cs
+++ Assets/Script/ArrowGroupTrigger.cs
@@ -3,10 +3,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
 
 public class ArrowGroupTrigger : MonoBehaviour
 {
 #region Fields
+  [ Title( "Setup" ) ]
+    [ SerializeField ] IntGameEvent event_arrow_spawn;
+    [ SerializeField ] PoolArrowGroupTrigger pool_arrow_trigger_group;
+
+  [ Title( "Setup" ) ]
+    [ SerializeField ] Collider arrow_group_trigger_collider;
+
+// Private
+    private int arrow_group_trigger_index;
 #endregion
 
 #region Properties
@@ -16,6 +27,22 @@ public class ArrowGroupTrigger : MonoBehaviour
 #endregion
 
 #region API
+    public void Spawn( int index, Vector3 position )
+    {
+		transform.position        = position;
+		arrow_group_trigger_index = index;
+
+		arrow_group_trigger_collider.enabled = true;
+	}
+
+    public void OnTrigger()
+    {
+		event_arrow_spawn.Raise( arrow_group_trigger_index );
+
+		arrow_group_trigger_collider.enabled = false;
+
+		pool_arrow_trigger_group.ReturnEntity( this );
+	}
 #endregion
 
 #region Implementation
