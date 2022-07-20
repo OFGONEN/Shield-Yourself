d6bc4009f8442898e2c9932426d0ac9d5413bd3a
ofgonen <ofgonen@gmail.com>
2022-07-20 17:33:55 +0300

    Create ArrowSystem script

---
+++ Assets/Script/ArrowSystem.cs
@@ -0,0 +1,38 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ CreateAssetMenu( fileName = "system_arrow", menuName = "FF/System/Arrow" ) ]
+public class ArrowSystem : ScriptableObject
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void SpawnArrowGroup( ArrowGroupDataGameEvent gameEvent )
+    {
+
+    }
+
+    public void OnLevelFinished()
+    {
+
+    }
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
