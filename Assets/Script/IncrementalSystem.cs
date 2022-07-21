d044b6ff0b8792b8bf4fb33af3a786d7015c1c98
ofgonen <ofgonen@gmail.com>
2022-07-21 17:21:17 +0300

    Implement IncrementalSystem

---
+++ Assets/Script/IncrementalSystem.cs
@@ -0,0 +1,60 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+public abstract class IncrementalSystem< Incremental > : MonoBehaviour
+{
+#region Fields
+  [ Title( "Setup" ) ]
+    [ SerializeField ] Incremental[] incremental;
+    [ SerializeField ] string incremental_name;
+
+  [ Title( "Setup" ) ]
+    [ SerializeField ] SharedFloatNotifier currency;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public bool CanUnlock()
+    {
+		var currentIndex = PlayerPrefsUtility.Instance.GetInt( incremental_name, 0 );
+
+		var lastIndex = currentIndex == incremental.Length - 1;
+
+		var canAfford = currency.sharedValue > Cost();
+		return !lastIndex && canAfford;
+	}
+
+    public void Unlock()
+    {
+		var currentIndex = PlayerPrefsUtility.Instance.GetInt( incremental_name, 0 );
+		PlayerPrefsUtility.Instance.SetInt( incremental_name, currentIndex + 1 );
+
+		currency.SharedValue -= Cost();
+	}
+
+    public Incremental CurrentIncremental()
+    {
+		return incremental[ PlayerPrefsUtility.Instance.GetInt( incremental_name, 0 ) ];
+	}
+
+    public abstract int Cost();
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
