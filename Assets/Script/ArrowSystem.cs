af0f7acb8f1dfbe86c7918e3bb8bc4cbaa43f3e9
ofgonen <ofgonen@gmail.com>
2022-07-26 12:59:18 +0300

    ArrowSystem: Refactor arrow spawn event configuration

--- Assets/Script/ArrowSystem.cs
+++ Assets/Script/ArrowSystem.cs
@@ -26,32 +26,36 @@ public class ArrowSystem : ScriptableObject
 #endregion
 
 #region API
-    public void SpawnArrowGroup( ArrowGroupDataGameEvent gameEvent )
-    {
-		// Setup sequence
-		var recycledSequence = pool_recycled_sequence.GetEntity();
-		recycledSequence.Recycle( () => OnSpawnSequenceComplete( recycledSequence ) );
+	public void SpawnArrows()
+	{
 
-		sequence_active.Add( recycledSequence.ID, recycledSequence );
-		var sequence = recycledSequence.Sequence;
-
-		// Shoot Delay
-		var delay = gameEvent.eventValue.arrow_shoot_delay;
-		var arrowGroup = gameEvent.eventValue.arrow_shoot_group;
-
-		sequence.AppendInterval( delay );
-
-		for( var i = 0; i < arrowGroup.Length; i++ )
-        {
-			var data = arrowGroup[ i ];
-			sequence.AppendInterval( arrowGroup[ i ].arrow_spawn_delay );
-			sequence.AppendCallback( () => SpawnArrow( data.arrow_spawn_height, data.arrow_speed ) );
-		}
-
-        // Spawn Arrow indicator
-		var uiArrowIndicator = pool_ui_arrow_indicator.GetEntity();
-		uiArrowIndicator.Spawn( gameEvent.eventValue.arrow_indicator_height, delay );
 	}
+    // public void SpawnArrowGroup( ArrowGroupDataGameEvent gameEvent )
+    // {
+	// 	// Setup sequence
+	// 	var recycledSequence = pool_recycled_sequence.GetEntity();
+	// 	recycledSequence.Recycle( () => OnSpawnSequenceComplete( recycledSequence ) );
+
+	// 	sequence_active.Add( recycledSequence.ID, recycledSequence );
+	// 	var sequence = recycledSequence.Sequence;
+
+	// 	// Shoot Delay
+	// 	var delay = gameEvent.eventValue.arrow_shoot_delay;
+	// 	var arrowGroup = gameEvent.eventValue.arrow_shoot_group;
+
+	// 	sequence.AppendInterval( delay );
+
+	// 	for( var i = 0; i < arrowGroup.Length; i++ )
+    //     {
+	// 		var data = arrowGroup[ i ];
+	// 		sequence.AppendInterval( arrowGroup[ i ].arrow_spawn_delay );
+	// 		sequence.AppendCallback( () => SpawnArrow( data.arrow_spawn_height, data.arrow_speed ) );
+	// 	}
+
+    //     // Spawn Arrow indicator
+	// 	var uiArrowIndicator = pool_ui_arrow_indicator.GetEntity();
+	// 	uiArrowIndicator.Spawn( gameEvent.eventValue.arrow_indicator_height, delay );
+	// }
 
     public void OnLevelFinished()
     {
